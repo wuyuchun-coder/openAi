585e1774c99f5a0a24a3e3a4797957fe72df11c7
伍宇淳 <553367003@qq.com>
2024-05-08 18:12:54 +0800

    openAi项目构建

---
+++ openAi-interfaces/src/test/java/com/example/openAi/api/ApiTest.java
@@ -0,0 +1,14 @@
+package com.example.openAi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class ApiTest {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication.class,args);
+    }
+}
